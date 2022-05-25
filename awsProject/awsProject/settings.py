b2fa7ef519391b2e7267b99cc94c823366cee52c
JeongSu-JI <jjs9536@gmail.com>
2022-05-25 10:43:27 +0900

    Update settings.py

--- awsProject/awsProject/settings.py
+++ awsProject/awsProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-15#c^mx%0pz0_!gb7k8al53k2+64_y*^9zwh-#_@nbb(bdcp0b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
